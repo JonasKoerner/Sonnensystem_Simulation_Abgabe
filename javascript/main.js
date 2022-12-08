4e7bdf8aa1e2da2a4faa6e3c7cced9f44aa80628
JonasKoerner <jonas_koerner@gmx.net>
2022-12-08 22:30:34 +0100

    test test test

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-import * as THREE from "../lib/three.module.js";
+import * as THREE from "../lib/three.module.js"; //test
 import {OrbitControls} from "../lib/OrbitControls.js";
 
 // radius: Größe des Planeten
