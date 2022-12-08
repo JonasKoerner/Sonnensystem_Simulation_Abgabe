b6eb2aec5ddfd93503222fe446d8954d8e62128b
kaibu <kaibuchholz13@gmail.com>
2022-12-08 22:26:32 +0100

    Button Minigame

--- javascript/main.js
+++ javascript/main.js
@@ -4,12 +4,13 @@ import {OrbitControls} from "../lib/OrbitControls.js";
 // radius: Größe des Planeten
 // position: Position im Raum
 // textureFile: Bild welches auf der Kugel dargestellt wird
-function erzeugePlanet(radius, position, textureFile) {
+function erzeugePlanet(radius, position, textureFile, name) {
     const geometry = new THREE.SphereGeometry(radius);
     const texture = new THREE.TextureLoader().load(textureFile);
     const material = new THREE.MeshBasicMaterial({map: texture});
     let mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(position.x, position.y, position.z);
+    mesh.name = name;
     return mesh;
 }
 
@@ -41,6 +42,7 @@ let renderer = new THREE.WebGLRenderer({
     canvas: document.getElementById("ThreeJsCanvas"),
     antialias: true,
 });
+
 // Größe im Browser
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -52,17 +54,17 @@ let controls = new OrbitControls(camera, renderer.domElement);
 window.addEventListener("resize", () => onWindowResize(camera, renderer), false);
 
 // Alle Objekte der Animation hinzufügen:
-const sunMesh = erzeugePlanet(8, new THREE.Vector3(0, 0, 0), "../img/sun.jpeg");
-const mercuryMesh = erzeugePlanet(2,  new THREE.Vector3(16, 0, 0), "../img/mercury.png");
-const venusMesh = erzeugePlanet(3, new THREE.Vector3(32, 0, 0), "../img/venus.jpeg");
-const earthMesh = erzeugePlanet(4, new THREE.Vector3(48, 0, 0), "../img/earth.jpeg");
-const marsMesh = erzeugePlanet(3, new THREE.Vector3(64, 0, 0), "../img/mars.jpeg");
-const jupiterMesh = erzeugePlanet(7, new THREE.Vector3(80,0,0),"../img/jupiter.png");
-const saturnMesh = erzeugePlanet(6, new THREE.Vector3(96,0,0), "../img/saturn.png");
-const uranusMesh = erzeugePlanet(5, new THREE.Vector3(112,0,0), "../img/uranus.png");
-const neptuneMesh = erzeugePlanet(5, new THREE.Vector3(128,0,0), "../img/neptune.png");
+const sonneMesh = erzeugePlanet(8, new THREE.Vector3(0, 0, 0), "../img/sun.jpeg", "sonne");
+const merkurMesh = erzeugePlanet(2, new THREE.Vector3(16, 0, 0), "../img/mercury.png", "merkur");
+const venusMesh = erzeugePlanet(3, new THREE.Vector3(32, 0, 0), "../img/venus.jpeg", "venus");
+const erdeMesh = erzeugePlanet(4, new THREE.Vector3(48, 0, 0), "../img/earth.jpeg", "erde");
+const marsMesh = erzeugePlanet(3, new THREE.Vector3(64, 0, 0), "../img/mars.jpeg", "mars");
+const jupiterMesh = erzeugePlanet(7, new THREE.Vector3(80, 0, 0), "../img/jupiter.png", "jupiter");
+const saturnMesh = erzeugePlanet(6, new THREE.Vector3(96, 0, 0), "../img/saturn.png", "saturn");
+const uranusMesh = erzeugePlanet(5, new THREE.Vector3(112, 0, 0), "../img/uranus.png", "uranus");
+const neptunMesh = erzeugePlanet(5, new THREE.Vector3(128, 0, 0), "../img/neptune.png", "neptun");
 
-scene.add(sunMesh, mercuryMesh, venusMesh, earthMesh, marsMesh, jupiterMesh, saturnMesh, uranusMesh, neptuneMesh);
+scene.add(sonneMesh, merkurMesh, venusMesh, erdeMesh, marsMesh, jupiterMesh, saturnMesh, uranusMesh, neptunMesh);
 
 let raycaster = new THREE.Raycaster();
 document.body.onmousedown = function (event) {
@@ -80,70 +82,109 @@ document.body.onmousedown = function (event) {
         controls.target = intersects[0].object.position;
         controls.update();
 
-        // ToDo: Steckbrief auf Seite anzeigen
-        // document.getElementById("Steckbrief")...
+        document.getElementById("steckbrief-" + sonneMesh.name).style.display = "none";
+        document.getElementById("steckbrief-" + merkurMesh.name).style.display = "none";
+        document.getElementById("steckbrief-" + venusMesh.name).style.display = "none";
+        document.getElementById("steckbrief-" + erdeMesh.name).style.display = "none";
+        document.getElementById("steckbrief-" + marsMesh.name).style.display = "none";
+        document.getElementById("steckbrief-" + jupiterMesh.name).style.display = "none";
+        document.getElementById("steckbrief-" + saturnMesh.name).style.display = "none";
+        document.getElementById("steckbrief-" + uranusMesh.name).style.display = "none";
+        document.getElementById("steckbrief-" + neptunMesh.name).style.display = "none";
+        document.getElementById("steckbrief-" + intersects[0].object.name).style.display = "block";
     }
 };
-
+//test//
 function berechneSonnenRotation(t, mesh) {
-    mesh.position.set(t, t, t);
-    mesh.rotation.set(t, t, t);
+    mesh.rotation.set(5*t, 0, 0);
 }
 
 function berechneMerkurPositionUndRotation(t, mesh) {
-    mesh.position.set(t, t, t);
-    mesh.rotation.set(t, t, t);
+    let r1 = 10.0;
+    let v1 = 1
+    let x1 = r1*Math.cos(v1*t);
+    let z1 = r1*Math.sin(v1*t);
+    mesh.position.set(x1, 0, z1);
+    mesh.rotation.set(5*t, 0, 0);
 }
 
 function berechneVenusPositionUndRotation(t, mesh) {
-    mesh.position.set(t, t, t);
-    mesh.rotation.set(t, t, t);
+    let r2 = 15.0;
+    let v2 = 1;
+    let x2 = r2*Math.cos(v2*t);
+    let z2 = r2*Math.sin(v2*t);
+    mesh.position.set(x2, 0, z2);
+    mesh.rotation.set(5*t, 0, 0);
 }
 
 function berechneErdPositionUndRotation(t, mesh) {
-    mesh.position.set(t, t, t);
-    mesh.rotation.set(t, t, t);
+    let r3 = 20.0;
+    let v3 = 1;
+    let x3 = r3*Math.cos(v3*t);
+    let z3 = r3*Math.sin(v3*t);
+    mesh.position.set(x3, 0, z3);
+    mesh.rotation.set(5*t, 0, 0);
 }
 
 function berechneMarsPositionUndRotation(t, mesh) {
-    mesh.position.set(t, t, t);
-    mesh.rotation.set(t, t, t);
+    let r4 = 25.0;
+    let v4 = 1;
+    let x4 = r4*Math.cos(v4*t);
+    let z4 = r4*Math.sin(v4*t);
+    mesh.position.set(x4, 0, z4);
+    mesh.rotation.set(5*t, 0, 0);
 }
 
 function berechneJupiterPositionUndRotation(t, mesh) {
-    mesh.position.set(t, t, t);
-    mesh.rotation.set(t, t, t);
+    let r5 = 30.0;
+    let v5 = 1;
+    let x5 = r5*Math.cos(v5*t);
+    let z5 = r5*Math.sin(v5*t);
+    mesh.position.set(x5, 0, z5);
+    mesh.rotation.set(5*t, 0, 0);
 }
 
 function berechneSaturnPositionUndRotation(t, mesh) {
-    mesh.position.set(t, t, t);
-    mesh.rotation.set(t, t, t);
+    let r6 = 35.0;
+    let v6 = 1;
+    let x6 = r6*Math.cos(v6*t);
+    let z6 = r6*Math.sin(v6*t);
+    mesh.position.set(x6, 0, z6);
+    mesh.rotation.set(5*t, 0, 0);
 }
 
 function berechneUranusPositionUndRotation(t, mesh) {
-    mesh.position.set(t, t, t);
-    mesh.rotation.set(t, t, t);
+    let r7 = 40.0;
+    let v7 = 1;
+    let x7 = r7*Math.cos(v7*t);
+    let z7 = r7*Math.sin(v7*t);
+    mesh.position.set(x7, 0, z7);
+    mesh.rotation.set(5*t, 0, 0);
 }
 
 function berechneNeptunPositionUndRotation(t, mesh) {
-    mesh.position.set(t, t, t);
-    mesh.rotation.set(t, t, t);
+    let r8 = 45.0;
+    let v8 = 1;
+    let x8 = r8*Math.cos(v8*t);
+    let z8 = r8*Math.sin(v8*t);
+    mesh.position.set(x8, 0, z8);
+    mesh.rotation.set(5*t, 0, 0);
 }
 
 let t = 0;
 // Wie schnell sich die Planeten um die Sonne bewegen sollen
 let speed = 1 / 60;
 const animate = () => {
-    t += speed;
-    berechneSonnenRotation(t, sunMesh);
-    berechneMerkurPositionUndRotation(t, mercuryMesh);
+    t += 1/60;
+    berechneSonnenRotation(t, sonneMesh);
+    berechneMerkurPositionUndRotation(t, merkurMesh);
     berechneVenusPositionUndRotation(t, venusMesh);
-    berechneErdPositionUndRotation(t, earthMesh);
+    berechneErdPositionUndRotation(t, erdeMesh);
     berechneMarsPositionUndRotation(t, marsMesh);
     berechneJupiterPositionUndRotation(t, jupiterMesh);
     berechneSaturnPositionUndRotation(t, saturnMesh);
     berechneUranusPositionUndRotation(t, uranusMesh);
-    berechneNeptunPositionUndRotation(t, neptuneMesh);
+    berechneNeptunPositionUndRotation(t, neptunMesh);
 
     // In jedem Frame die Animation neu rendern
     renderer.render(scene, camera);
---
+++ javascript/minigame.js
@@ -0,0 +1,84 @@
+var reihe = 3;
+var spalten = 3;
+
+var aktuellesTeil;
+var platzhalterTeil;
+
+var zuege = 0;
+
+var reihenfolgeBilder = ["2", "7", "5", "1", "8", "6", "3", "9", "4"]
+
+window.onload = function(){
+    for (let r=0; r< reihe; r++){
+        for(let s=0; s< spalten; s++){
+
+            let teil = document.createElement("img");
+            teil.id = r.toString() + "-" + s.toString();
+            teil.src = /img/ + reihenfolgeBilder.shift() + ".jpg"
+
+            teil.addEventListener("dragstart", dragStart); //anklicken eines Bildes um es zu bewegen
+            teil.addEventListener("dragover", dragOver); //bewegen der Maus während man ein Bild angeklickt hat
+            teil.addEventListener("dragenter", dragEnter); //bewegen eines Bildes auf ein anderes
+            teil.addEventListener("dragleave", dragLeave); // verlassen des alten Platzes
+            teil.addEventListener("drop", dragDrop);  //bewegen eines Bildes auf ein anderen und los und lassen
+            teil.addEventListener("dragend", dragEnd); //nach dragDrop, tauschen der Bilder
+
+            document.getElementById("oberflaeche").append(teil)
+        }
+    }
+}
+
+function dragStart() {
+    aktuellesTeil = this; //dieses Bild wird bewegt
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    platzhalterTeil = this; //this refers to the img tile being dropped on
+}
+
+function dragEnd() {
+
+    if (!platzhalterTeil.src.includes("4.jpg")){
+        return;
+    }
+
+    var aktPosition = aktuellesTeil.id.split("-"); // 0-0 -> ["0","0"]
+    var r = parseInt(aktPosition[0]);
+    var s = parseInt(aktPosition[1]);
+
+    var anderePosition = platzhalterTeil.id.split("-");
+    var r2 = parseInt(anderePosition[0]);
+    var s2 = parseInt(anderePosition[1]);
+
+    var bewegenlinks = r === r2 && s2 === s-1;
+    var bewegenrechts = r === r2 && s2 === s+1;
+
+    var bewegenhoch = s === s2 && r-1;
+    var bewegenrunter = s=== s2 && r+1;
+
+    var isAdjacent = bewegenlinks || bewegenrechts || bewegenhoch || bewegenrunter
+
+    if (isAdjacent){
+        var aktuellesTeilImg = aktuellesTeil.src;
+        var platzhalterImg = platzhalterTeil.src;
+
+        aktuellesTeil.src = platzhalterImg;
+        platzhalterTeil.src = aktuellesTeilImg;
+
+        zuege += 1;
+        document.getElementById("Züge").innerText = zuege
+    }
+
+}
